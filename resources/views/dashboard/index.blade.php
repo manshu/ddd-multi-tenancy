1d34944608b6030357b57fd1b6028ee085837ec0
Himanshu Batra <himanshu@batra.io>
2023-08-21 14:44:56 -0400

    WiP

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <div>I am dashboard</div>
-    <div>
-        <a href="{{ route('tenant.logout') }}"
-            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-    </div>
+<div>I am dashboard</div>
+<div>
+    <a href="{{ route('tenant.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        Logout
+    </a>
+    <form id="logout-form" action="{{ route('tenant.logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
+</div>
 @endsection
